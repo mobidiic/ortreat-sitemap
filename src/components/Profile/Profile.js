96f7a98e9948a3f7d9e0bf01d4003cb21eba16de
cean_park <bagsihyeon@bagsihyeon-ui-MacBookPro.local>
2021-02-03 23:18:35 +0900

    set imgs and change style

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 import style from "./Profile.module.scss";
+import img from "../../assets/images/img_main.jpg";
 
 const Profile = () => {
     return (
-        <div className={style.profile__card}>
-            <div className={style.profile__title}>
-                Ort Bakery
+        <Fragment>
+            <div className={style.profile__img} >
+                    <img src={img} alt="ortreat cake" />
+                </div>
+            <div className={style.profile__card}>
+                <div className={style.profile__title}>
+                    Ort Bakery
+                </div>
+                <div className={style.profile__desc}>
+                    <p>pet cake shop</p>
+                </div>
             </div>
-            <div className={style.profile__desc}>
-                <p>pet cake shop</p>
-            </div>
-        </div>
+        </Fragment>
     )
 };
 
