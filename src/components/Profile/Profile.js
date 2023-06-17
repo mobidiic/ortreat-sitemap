c807a7891d0400663282865b027a78c37d2be4b0
cean_park <bagsihyeon@bagsihyeon-ui-MacBookPro.local>
2023-06-17 15:30:23 +0900

    change image and site list

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,20 +1,12 @@
 import React, { Fragment } from "react";
 import style from "./Profile.module.scss";
-import img from "../../assets/images/img_main.jpg";
+import img from "../../assets/images/img_main_v2.jpg";
 
 const Profile = () => {
     return (
         <Fragment>
             <div className={style.profile__img} >
                     <img src={img} alt="ortreat cake" />
-                </div>
-            <div className={style.profile__card}>
-                <div className={style.profile__title}>
-                    Ort Bakery
-                </div>
-                <div className={style.profile__desc}>
-                    <p>pet cake shop</p>
-                </div>
             </div>
         </Fragment>
     )
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -6,7 +6,7 @@ const Main = () =>  (
     <Fragment>
         <Profile />
         <SiteList />
-        <Location />
+        {/* <Location /> */}
     </Fragment>
     );
 
